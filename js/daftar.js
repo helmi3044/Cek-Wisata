71676ffc01ae9eb5b69a8cd3afe28d90f34fcc6c
helmi3044 <56855437+helmi3044@users.noreply.github.com>
2020-12-23 21:15:41 +0700

    Create daftar.js

---
+++ js/daftar.js
@@ -0,0 +1,10 @@
+/* menambahkan pop up konfirmasi ketika melakukan daftar akun */
+function daftar() {
+  var daftar = confirm("Apakah anda yakin ingin mendaftarkan akun anda?")
+
+  if (daftar) {
+    window.location = "index.html"
+  } else {
+    window.location = "PROJEK/daftar.html"
+  }
+}
