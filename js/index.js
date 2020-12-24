9d6c84ab75f280b2f4b76aee699cd84ed437796e
helmi3044 <56855437+helmi3044@users.noreply.github.com>
2020-12-24 12:01:57 +0700

    Update index.js
    menambahkan apabila search digunakan bisa direct ke halaman tertentu sesuai input

--- js/index.js
+++ js/index.js
@@ -8,3 +8,18 @@ if (time < 10) {
 } else {
   alert("Selamat malam, selamat berkunjung di halaman Web Cek Wisata");
 }          
+
+/* apabila search digunakan bisa direct ke halaman tertentu sesuai input */
+function terimainput() {
+  var cari = document.forms["cari"]["search"].value;
+
+  if (cari == "info wisata" || cari == "Info wisata" || cari == "info" || cari == "wisata") {
+    window.location = "info.html"
+  } else if (cari == "info penginapan" || cari == "penginapan" || cari == "hotel") {
+    window.location = "penginapan.html"
+  } else if (cari == "Review" || cari == "review" || cari == "Review wisata") {
+    window.location = "review.html"
+  } else {
+     window.location = "index.html"
+  }
+}
